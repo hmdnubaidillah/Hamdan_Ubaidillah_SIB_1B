ad070d98eb636d75fe8ffb53dad0e9cad947d9ee
HamdanUbaidillah <daniuya71@gmail.com>
2023-10-05 15:14:42 +0700

    fix: percobaan1

--- dasproPraktikum/jobsheet6/percobaan1/Pemilihan2Percobaan1_17.java
+++ dasproPraktikum/jobsheet6/percobaan1/Pemilihan2Percobaan1_17.java
@@ -5,13 +5,13 @@ class Pemilihan2Percobaan1_17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        while (true) {
+        
             int tahun = input.nextInt();
 
             if (tahun % 400 == 0) {
                 System.out.println("Tahun kabisat");
             } else if (tahun % 100 == 0) {
-                System.out.println("Tahun kabisat");
+                System.out.println("Bukan tahun kabisat");
             } else if (tahun % 4 == 0) {
                 System.out.println("Tahun kabisat");
             } else {
@@ -20,5 +20,3 @@ class Pemilihan2Percobaan1_17 {
         }
         
     }
-
-}
